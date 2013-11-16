a056ded249a2f7d7cc56e6e5faf50f6e1d4f6609
Joe Borg <jdb@sitrep3.com>
2013-11-16 12:02:15 +0000

    Preparing for PiPy.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyDueDil',
-    version='0.1.0',
+    version='0.1.1',
     packages=['PyDueDil'],
     url='https://github.com/joedborg/PyDueDil',
     license='GPLv2',
